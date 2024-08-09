1982b6f89a6460826ca814dc6949892c90150d30
artreiner88 <artur.reiner88@gmail.com>
2024-08-09 12:44:31 +0200

    Initial Commit

---
+++ SwiftUIStackView/SwiftUIStackViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStackViewApp.swift
+//  SwiftUIStackView
+//
+//  Created by Artur Reiner on 09.08.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStackViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
